30814265496c8cba629f5671837e5c8a842c419c
Gowtham <gowtham@gmail.com>
2026-02-04 18:56:43 +0530

    fix: adjust padding class for Header component for layout consistency

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,7 @@
 
 export default function Header() {
 	return (
-		<div className="pt-md-6 pt-4 space-y-2 text-center">
+		<div className="pt-md-6 pt-2 space-y-2 text-center">
 			<div className="pt-6 space-y-2 text-center">
 				<h1 className="dark:text-white font-extrabold text-4xl tracking-tight uppercase">
 					Applied Jobs Tracker
